1d836bdaa93ce8e500c9537ba262e64a169eb41c
silasjul <silaskierstein@gmail.com>
2025-07-21 17:01:51 +0200

    cool background shader

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,16 +2,18 @@
 import React from "react";
 import { motion } from "framer-motion";
 import Navbar from "./Navbar";
+import VantaFog from "./VantaFog";
 
 export default function Hero() {
     return (
-        <div className="relative min-h-screen overflow-hidden ml">
+        <div className="relative min-h-screen overflow-x-hidden">
             <Navbar />
-            <motion.h1 className="text-9xl mt-[10%] ml-8">
+            <motion.h1 className="text-9xl mt-[10%] mb-[10%] ml-8">
                 Optimized websites,
                 <br />
                 powerful automations.
             </motion.h1>
+            <VantaFog />
         </div>
     );
 }
---
+++ src/components/VantaFog.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import Script from "next/script";
+
+export default function VantaFog() {
+    return (
+        <div className="h-screen bg-linear-to-b from-white to-20% to-transparent">
+            <div
+                className="absolute -z-1 w-screen h-screen"
+                id="vanta-fog"
+            ></div>
+            <Script
+                src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r134/three.min.js"
+                strategy="beforeInteractive"
+            />
+            <Script
+                src="https://cdn.jsdelivr.net/npm/vanta@latest/dist/vanta.fog.min.js"
+                strategy="beforeInteractive"
+            />
+            <Script id="script">
+                {`VANTA.FOG({
+      el: "#vanta-fog",
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+    });`}
+            </Script>
+        </div>
+    );
+}
